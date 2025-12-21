3825cf32cb431495a43fcdee3827e6d4c7aca647
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2025-12-21 10:26:25 -0600

    fixed proof_of_work

--- blockchain.py
+++ blockchain.py
@@ -53,7 +53,7 @@ class BlackChain(object):
     def proof_of_work(self):
         transaction = self.transaction_pool.copy()
         previous_hash = self.hash(self.chain[-1])
-        nonce += 1
+        nonce = 0
         while self.valid_proof(transaction, previous_hash, nonce) is False:
             nonce += 1
         return nonce
@@ -68,8 +68,8 @@ if __name__ == '__main__':
     block_chain.create_block(nonce, previous_hash)
     utils.pprint(block_chain.chain)
     
-    block_chain.add_transaction('C', 'D', 1.0)
-    block_chain.add_transaction('X', 'Y', 1.0)
+    block_chain.add_transaction('C', 'D', 2.0)
+    block_chain.add_transaction('X', 'Y', 3.0)
     previous_hash = block_chain.hash(block_chain.chain[-1])
     nonce = block_chain.proof_of_work()
     block_chain.create_block(nonce, previous_hash)
