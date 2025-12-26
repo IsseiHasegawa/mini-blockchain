5bda10a5c501d4c78ef9a5383c966ddd3d8fc037
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2025-12-26 12:47:54 -0600

    Create file for server(flask)

---
+++ blockchain_server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import jsonify
+
+import blockchain
+import wallet
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000,
+                        type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.config['port'] = port
+
+    app.run(host = '0.0.0.0', port=port, threaded=True, debug=True)
